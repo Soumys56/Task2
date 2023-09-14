a8c6a15b27ba022854618b9a15b4ecbc1cbd25b5
Soumys56 <88768160+Soumys56@users.noreply.github.com>
2023-09-14 16:13:12 +0530

    Task1

---
+++ Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Kitkat & Dairy Milk ",
+    price: 45,
+    desc: "Kitkat & Dairy Milk clebration Combo",
+    img: "https://i.pinimg.com/originals/4c/f7/b8/4cf7b8fa13224525d7a0a5480c4cb56d.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Chocolate Combo",
+    price: 100,
+    desc: "Chocolate Combo Celebration",
+    img: "https://i.pinimg.com/originals/73/09/20/73092074e0675fe484233e9f2b3880f0.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "2-1 Chocolate Combo",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "https://c8.alamy.com/comp/EY4J0H/a-box-of-nestle-chocolate-bar-selection-EY4J0H.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Kitkat and Snack",
+    price: 300,
+    desc: "Kitkat and Snack combo",
+    img: "https://images.unsplash.com/photo-1574183878891-e08ec44abc37?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hvY29sYXRlJTIwY2FuZHl8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+  },
+];
